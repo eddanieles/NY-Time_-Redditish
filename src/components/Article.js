389cf754f45fc1843d705242692cfa5f2e9599c5
eddanieles <ed.danieles@yahoo.com>
2016-09-12 10:25:25 -0400

    added axios method

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class Article extends Component{
   render(){
     return(
-      <h1>Article page</h1>
+      <article>
+        <a href={this.props.webUrl}>{this.props.webUrl}</a>
+        <p>{this.props.headline}</p>
+        <p>{this.props.snippet}</p>
+        {/* <Link to="/article">Read More</Link> */}
+        {/* <button className="btn btn-success">Upvote</button>
+        <button className="btn btn-danger">Downvote</button> */}
+      </article>
     )
   }
 }
--- src/components/Front.js
+++ src/components/Front.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
-import Search from './Search'
+import Search from './Search';
+import axios from 'axios';
+import Article from './Article';
 
 class Front extends Component {
   constructor(props){
     super(props);
     this.state = {
-      searchTerms: 'news',
+      searchTerms: 'donald trump',
+      response: []
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getServerData = this.getServerData.bind(this);
   }
   handleSubmit(event){
     event.preventDefault();
+    console.log(this.state.response);
     //console.log(event.target.elements[0].value);
     let input = event.target.elements[0];
     let inputValue = input.value;
     this.setState({
       searchTerms: inputValue
     });
+    this.getServerData();
+  }
+  getServerData(){
+    axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=d52b04cdb0eb449988f46e0036017e2d&q=${this.state.searchTerms}&sort=newest`)
+      .then(
+        function (result) {
+          //console.log(result.length);
+          this.setState({
+            response: result.data.response.docs
+          });
+        }.bind(this))
+  }
+  componentDidMount(){
+    this.getServerData();
   }
   render() {
+    const articles = this.state.response.map(article => <Article webUrl={article.web_url} headline={article.headline.main} snippet={article.snippet}/>)
     return (
       <div>
         <form className="navbar-form" onSubmit={this.handleSubmit}>
@@ -27,7 +47,8 @@ class Front extends Component {
           </div>
           <button type="submit" className="btn btn-default">Submit</button>
         </form>
-        <Search searchTerms={this.state.searchTerms}/>
+        {/* <Search searchTerms={this.state.searchTerms}/> */}
+        {articles}
       </div>
     )
   }
